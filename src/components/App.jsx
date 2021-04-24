b90a7921d55add187fb34085e29ec4d5a1eb9543
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2021-04-23 19:57:55 -0500

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Form from "./Form";
+import Input from "./Input";
 
 var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      <Form />
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
